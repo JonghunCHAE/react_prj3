8f6ea40ddb4a6cb4e3befd605e18b9917de4f1d2
chaejonghun <ace1373@naver.com>
2021-01-27 00:54:41 +0900

    310페이지까지 끝

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,11 +7,11 @@ import {
 import cn from "classnames";
 import "./TodoListItem.scss";
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   return (
-    <div className="TodoListItem">
+    <div className="TodoListItem-virtualized" style={style}>
       <div className={cn("checkbox", { checked })} onClick={() => onToggle(id)}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
@@ -23,4 +23,7 @@ const TodoListItem = ({ todo, onRemove, onToggle }) => {
   );
 };
 
-export default TodoListItem;
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo
+);
